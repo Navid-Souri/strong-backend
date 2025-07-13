022446b979e8e029b62e55ebe6770732246e45b8
Navid Souri <nasiruos9013@gmail.com>
2025-07-13 18:48:55 +0330

    Add build.sh script for Render deployment

---
+++ build.sh
@@ -0,0 +1,14 @@
+    bash
+    #!/usr/bin/env bash
+    # Exit on error
+    set -o errexit
+
+    # Install Python dependencies
+    pip install -r requirements.txt
+
+    # Collect static files
+    python manage.py collectstatic --no-input
+
+    # Apply database migrations
+    python manage.py migrate
+  
